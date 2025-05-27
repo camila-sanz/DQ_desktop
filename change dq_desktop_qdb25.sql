c1b420b0b520bc8fcfdc0c3c30ac50d763ef8c58
camila-sanz <47565039+camila-sanz@users.noreply.github.com>
2025-05-27 09:06:51 +0200

    db and ontology

---
+++ change dq_desktop_qdb25.sql
@@ -0,0 +1,51 @@
+---- NO
+SELECT * FROM dq_appliedmethod_dqmodel_association;
+SELECT * FROM dq_dimension;
+SELECT * FROM dq_factor;
+SELECT * FROM dq_metric;
+SELECT * FROM dq_model;
+SELECT * FROM dq_model_ontology_association;
+SELECT * FROM dq_value_measure;
+SELECT * FROM dw_base_granularity;
+SELECT * FROM dw_cube;
+SELECT * FROM dw_measure;
+SELECT * FROM dw_value;
+SELECT * FROM dwcategory_dwbasegranularity;
+SELECT * FROM map_cat;
+SELECT * FROM map_dim;
+SELECT * FROM onto_class_ontology_association;
+SELECT * FROM precondition;
+SELECT * FROM precondition_dqmetric;
+
+
+
+
+---- SI
+
+
+SELECT * FROM dq_appmethod_dwatt;
+
+
+
+
+
+
+---- UPDATED
+SELECT * FROM dw_dimension;
+SELECT * FROM dw_datawarehouse;
+SELECT * FROM dw_hierarchy;
+SELECT * FROM dw_category;
+SELECT * FROM dw_category_position;
+SELECT * FROM dw_attribute;
+SELECT * FROM ontology;
+SELECT * FROM onto_data_type;
+SELECT * FROM onto_class;
+SELECT * FROM onto_data_property;
+SELECT * FROM onto_object_property;
+SELECT * FROM dq_method;
+SELECT * FROM dq_appmethod;
+SELECT * FROM mapping;
+SELECT * FROM map_att;
+SELECT * FROM map_hier;
+SELECT * FROM dq_appmethod_map_assoc;
+SELECT * FROM dq_improvement;
